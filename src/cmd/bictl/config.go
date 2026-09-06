8ddc5e1014ebd0b29408ced09e3b33bd795b4376
hollingv <hollingv@protonmail.com>
2026-09-06 09:43:20 -0400

    feat(build): place all generated files into TARGET_DIR=dist

--- src/cmd/bictl/config.go
+++ src/cmd/bictl/config.go
@@ -5,3 +5,6 @@ const siteName = "Bodily Integrity Commons"
 
 // projectPrefix is the acronym of siteName, used as a prefix for all environment variables.
 const projectPrefix = "BIC"
+
+// targetDir is the output directory for the generated site and binary.
+const targetDir = "dist"
--- src/server/config.go
+++ src/server/config.go
@@ -3,3 +3,6 @@ package main
 // projectPrefix is the prefix for all environment variables used by this project.
 // Change this value to rename all env vars in one place.
 const projectPrefix = "BIC"
+
+// targetDir is the directory from which the site is served.
+const targetDir = "dist"
