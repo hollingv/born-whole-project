a31da733254f267757830bd452d9b132386bc6df
hollingv <hollingv@protonmail.com>
2026-09-19 13:08:12 -0400

    feat(ui): add org image column to Events page

--- src/cmd/bwctl/internal/data/events.go
+++ src/cmd/bwctl/internal/data/events.go
@@ -2,16 +2,22 @@ package data
 
 // Event represents a single upcoming or past event.
 type Event struct {
-	Date        string
-	Description string
-	URL         string
+	Date          string
+	Description   string
+	URL           string
+	OrgName       string
+	OrgThumbnail  string
+	OrgWebsiteUrl string
 }
 
 // Events is the list of events to display on the events page.
 var Events = []Event{
 	{
-		Date:        "2026-09-14",
-		Description: "Historic Hadachek v. Oregon Court Hearing",
-		URL:         "https://www.tickettailor.com/events/intactglobal/2332513#",
+		Date:          "2026-09-14",
+		Description:   "Historic Hadachek v. Oregon Court Hearing",
+		URL:           "https://www.tickettailor.com/events/intactglobal/2332513",
+		OrgName:       "Intact Global",
+		OrgThumbnail:  "images/intactglobal.svg",
+		OrgWebsiteUrl: "https://intactglobal.org",
 	},
 }
